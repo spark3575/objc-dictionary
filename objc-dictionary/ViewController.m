df5aebafbc655c435b7fdff1b256f6126c0c95b4
Shin Park <shinpark@shins-mac-mini.home>
2017-04-04 09:38:30 -0500

    Objective-C NSDictionary & NSMutableDictionary

--- objc-dictionary/ViewController.m
+++ objc-dictionary/ViewController.m
@@ -16,7 +16,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSNumber *age = [NSNumber numberWithInt:40];
+    NSNumber *age2 = [NSNumber numberWithInt:34];
+    
+    NSDictionary *dict = @{@"jack": age, @"jon": age2};
+    NSDictionary *dict2 = [[NSDictionary alloc]init];
+    
+    int jacksAge = [[dict objectForKey:@"jack"] intValue];
+    
+    NSNumber *age3 = [dict objectForKey:@"jack"];
+    int jacksAge2 = [age3 intValue];
+    NSLog(@"Jacks age: %d", jacksAge);
+    
+    NSMutableDictionary *mut = [@{@"1": @"BMW", @"2": @"VOLVO", @"3": @"Ugly Honda Civic"} mutableCopy];
+    
+    NSMutableDictionary *mut2 = [[NSMutableDictionary alloc]init];
+    
+    [mut2 setObject:@"An Object" forKey:@"A key"];
+    [mut2 setObject:[NSNumber numberWithDouble:20.0000] forKey:@"doubletrouble"];
 }
 
 
